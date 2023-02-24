e1bbbc740c17286b058bfae236f61b8b7167c9e7
milenaramosc <milena.ramos@estudante.ufjf.br>
2023-02-24 15:44:56 -0300

    iguim

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,16 @@ $routes->match(['get','post'],'/sindromes', 'Sindromes::index',['filter'=>'filte
 // $routes->group('api', ['filter' => 'api-auth'], static function ($routes) {
 //     $routes->resource('users');//cria todos os metodos
 // });
+$routes->group('atendimentos', static function ($routes) {
+    $routes->post('create', 'Atendimentos::create');
+});
+
+// $router->post(
+//     '/questionario/inserir',
+//     fn (Request $request) => (new QuestionarioController())
+//     ->enviarDadosQuestionario($request->getJson())
+// );
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
---
+++ app/Models/Atendimentos.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Atendimentos extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'atendimentos';
+    protected $primaryKey       = 'id_atendimento';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'localizacao',
+        'ip',
+        'profissao_usuario'
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'data_hora';
+    protected $updatedField  = '';
+    protected $deletedField  = '';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    
+}
